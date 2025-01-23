d491ba5df83af663392b09cd2bde4cdb322e927c
fizzyseo <whatafineday23@gmail.com>
2025-01-23 04:24:37 +0000

    [Add] 03_Vanilla_RNN

---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def softmax(x):
+    xt = np.exp(x - np.max(x))
+    return xt / np.sum(xt)
+
+def save_model_parameters_theano(outfile, model):
+    U, V, W = model.U.get_value(), model.V.get_value(), model.W.get_value()
+    np.savez(outfile, U=U, V=V, W=W)
+   
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+    U, V, W = npzfile["U"], npzfile["V"], npzfile["W"]
+    model.hidden_dim = U.shape[0]
+    model.word_dim = U.shape[1]
+    model.U.set_value(U)
+    model.V.set_value(V)
+    model.W.set_value(W)
+    
